4bb0c20b827b020fcf953da1b506bbeed837d1bf
Shorya Sharma <hksharma8055@gmail.com>
2026-09-22 12:46:15 +0530

    3RD EXPERIMENT

---
+++ exp3.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(3008, () => {
+    console.log('Server chal raha hai port http://localhost:3008');
+});
